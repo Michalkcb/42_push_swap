5852a907d6b5052f67e5632f7f0873fb01702329
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-11-02 17:46:13 +0100

    finished sort 4-9

--- src/sort_for_4_to_9.c
+++ src/sort_for_4_to_9.c
@@ -6,7 +6,7 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 17:34:33 by mbany             #+#    #+#             */
-/*   Updated: 2024/11/02 16:33:56 by mbany            ###   ########.fr       */
+/*   Updated: 2024/11/02 17:45:03 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,53 @@
 
 void	sort_for_4_to_9(t_stack *stack_a, t_stack *stack_b)
 {
-	
+	while (stack_a->size > 3)
+		push_min_value_to_b(stack_a, stack_b);
+	sort_3_stack(stack_a);
+	while (stack_b->size > 0)
+		pa(stack_a, stack_b);
+}
+void	push_min_value_to_b(t_stack *stack_a, t_stack *stack_b)
+{
+	int min_index;
+	int rotations;
+
+	min_index = find_index_for_min_value(stack_a);
+	if (min_index < stack_a-> size / 2)
+	{
+		rotations = min_index;
+		while (rotations-- > 0)
+			ra(stack_a);
+	}
+	else
+	{
+		rotations = stack_a->size - min_index;
+		while (rotations-- > 0)
+			rra(stack_a);
+	}
+	pb(stack_a, stack_b);
+}
+
+int	find_index_for_min_value(t_stack *stack)
+{
+	int min_index;
+	int min_value;
+	int current_index;
+	t_node *current;
+
+	min_index = 0;
+	min_value = 0;
+	current = stack->top;
+	min_value = current->value;
+	while (current != NULL)
+	{
+		if (current->value < min_value)
+		{
+			min_value = current->value;
+			min_index = current_index;
+		}
+		current = current->next;
+		current_index++;
+	}
+	return (min_index);
 }
