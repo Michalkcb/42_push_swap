ac19a4238462234ac4728896b2f6b70a442204da
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-11-02 18:50:58 +0100

    add rotate

--- src/operations_rotate.c
+++ src/operations_rotate.c
@@ -6,7 +6,48 @@
 /*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 17:33:39 by mbany             #+#    #+#             */
-/*   Updated: 2024/10/31 17:33:40 by mbany            ###   ########.fr       */
+/*   Updated: 2024/11/02 18:50:38 by mbany            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/push_swap.h"
+
+void	ra(t_stack *stack_a)
+{
+	t_node *first;
+	t_node *last;
+
+	first = stack_a->top;
+	last = stack_a->top;
+	while (last->next != NULL)
+		last = last->next;
+	stack_a->top = first->next;
+	first->next = NULL;
+	last->next = first;
+	write(1,"ra\n",3);
+}
+void	ra(t_stack *stack_b)
+{
+	t_node *first;
+	t_node *last;
+
+	first = stack_b->top;
+	last = stack_b->top;
+	if (stack_b->top == NULL)
+		return ;
+	if (stack_b->top && stack_b->top->next)
+	{
+		while (last->next != NULL)
+			last = last->next;
+	}
+	stack_b->top = first->next;
+	first->next = NULL;
+	last->next = first;
+	write(1,"rb\n",3);
+}
+void	rr(t_stack *stack_a, t_stack *stack_b)
+{
+	ra(stack_a);
+	rb(stack_b);
+	write(1,"rr\n",3);
+}
